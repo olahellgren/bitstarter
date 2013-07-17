7aa7930d299fd173cbcfe3c3ce528ee849559e01
Ola Hellgren <ola.hellgren@yahoo.se>
2013-07-17 14:07:47 +0000

    buffer changes

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response) {
     var fs = require('fs');
     var buffer = new Buffer(fs.readFile('index.html'));
 
-    response.send(buffer.toString('utf8'));
+    response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
