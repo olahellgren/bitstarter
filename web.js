4da945c50eedc515c430533aed83862357763ea6
Ola Hellgren <ola.hellgren@yahoo.se>
2013-07-18 06:17:30 +0000

    Changed buffer to buf

--- web.js
+++ web.js
@@ -4,9 +4,9 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var fs = require('fs');
-    var buffer = new Buffer(fs.readFileSync('index.html'));
+    var buf = new Buffer(fs.readFileSync('index.html'));
 
-    response.send(buffer.toString('utf-8'));
+    response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
